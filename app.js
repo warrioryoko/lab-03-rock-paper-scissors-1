e6ab0321e38f77e70b54f6bdd4950eda8f672a08
Jerud-Moyer <jerudmoyer@gmail.com>
2020-07-08 17:01:32 -0700

    functions written and tested

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import functions and grab DOM elements
 import { getRandomThrow } from './rpsUtils.js';
+import { checkResult } from './rpsUtils.js';
 const playButton = document.querySelector('#play-button');
 const results = document.querySelector('#result');
 const wins = document.querySelector('#tally');
@@ -9,10 +10,25 @@ console.log(getRandomThrow());
 
 
 
+
+
+
 // initialize state
 let winsData = 0;
 let lossdata = 0;
+let drawsData = 0;
+
 
 
 
 // set event listeners to update state and DOM
+playButton.addEventListener('click', () => {
+    const player = document.querySelector('input:checked');
+    const computer = getRandomThrow();
+    console.log(player, computer);
+    const result = checkResult(player, computer);
+    if (result === 'win');
+    winsData ++;
+    results.style.textcontent = winsData;
+
+});
--- rpsUtils.js
+++ rpsUtils.js
@@ -9,4 +9,25 @@ export function getRandomThrow() {
         return hand = 'scissors';
     }
 }
-console.log(getRandomThrow);
+
+export function checkResult(player, computer) {
+    if (player === 'rock' && computer === 'paper') 
+        return 'loss';
+    if (player === 'rock' && computer === 'scissors')
+        return 'win';
+    if (player === 'rock' && computer === 'rock')
+        return 'draw';
+    if (player === 'paper' && computer === 'rock')
+        return 'win';
+    if (player === 'paper' && computer === 'paper')
+        return 'draw';
+    if (player === 'paper' && computer === 'scissors')
+        return 'loss';
+    if (player === 'scissors' && computer === 'rock')
+        return 'loss';
+    if (player === 'scissors' && computer === 'paper')
+        return 'win';
+    if (player === 'scissors' && computer === 'scissors')
+        return 'draw';
+
+}
